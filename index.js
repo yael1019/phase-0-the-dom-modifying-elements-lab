0c867a8ed08a2627f1cad76dad9560ce4ae7013d
Yael Harosh <yaelharosh01@gmail.com>
2023-01-12 12:08:31 -0500

    pass dom lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove('main');
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Yael is the champion';
+
+document.body.append(newHeader);
